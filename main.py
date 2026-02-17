80f9c04cbfb3e9a5fb1ee850c0c29a338aa25da7
nonsecularman <krishbharti3404@gmail.com>
2026-02-17 18:48:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,45 +1,5 @@
 import asyncio
-from pyrogram import Client
-from pytgcalls import PyTgCalls, idle
-
-API_ID = "YOUR_API_ID"
-API_HASH = "YOUR_API_HASH"
-SESSION_NAME = "my_account"
-
-
-# =========================
-# CLIENTS
-# =========================
-
-app = Client(
-    SESSION_NAME,
-    api_id=API_ID,
-    api_hash=API_HASH
-)
-
-pytgcalls = PyTgCalls(app)
-
-
-# =========================
-# START BOT
-# =========================
-
-async def start_bot():
-    print("🚀 Starting Bot...")
-
-    await app.start()
-    await pytgcalls.start()
-
-    me = await app.get_me()
-    print(f"✅ Logged in as: {me.first_name}")
-
-    print("🎵 Music Bot Running...")
-    await idle()
-
-
-# =========================
-# MAIN ENTRY
-# =========================
+from Zaid.main import start_bot
 
 if __name__ == "__main__":
     asyncio.run(start_bot())
