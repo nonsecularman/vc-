8c79018a9045a34023a162d09b918dfda2596bf2
nonsecularman <krishbharti3404@gmail.com>
2026-02-17 20:54:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
 import asyncio
-import signal
 from Zaid.main import start_bot
 
 
 async def main():
-    # bot start
     await start_bot()
 
-    # bot को exit होने से रोकता है
-    stop_event = asyncio.Event()
-
-    for sig in (signal.SIGINT, signal.SIGTERM):
-        asyncio.get_event_loop().add_signal_handler(
-            sig, stop_event.set
-        )
-
-    await stop_event.wait()
+    # loop alive रखेगा (py-tgcalls crash fix)
+    while True:
+        await asyncio.sleep(3600)
 
 
 if __name__ == "__main__":
