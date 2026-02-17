30807fc56307a2d86d0be504ecf58b2a990f8ee8
nonsecularman <krishbharti3404@gmail.com>
2026-02-17 21:03:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
 import asyncio
 from Zaid.main import start_bot
 
-
-async def main():
-    await start_bot()
-
-    # loop alive रखेगा (py-tgcalls crash fix)
-    while True:
-        await asyncio.sleep(3600)
-
-
 if __name__ == "__main__":
-    asyncio.run(main())
+    asyncio.run(start_bot())
