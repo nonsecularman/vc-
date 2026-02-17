9d72e37e10c1add42218f9eeedd02dacb179e70e
nonsecularman <krishbharti3404@gmail.com>
2026-02-17 21:13:08 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import asyncio
 from Zaid.main import start_bot
 
+async def main():
+    await start_bot()
+
+    # process को alive रखेगा (Heroku fix)
+    while True:
+        await asyncio.sleep(3600)
+
 if __name__ == "__main__":
-    asyncio.run(start_bot())
+    asyncio.run(main())
