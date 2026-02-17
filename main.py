1ab17ad96ce01206645007f0d640ba974da52a0f
nonsecularman <krishbharti3404@gmail.com>
2026-02-17 20:35:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
 import asyncio
-import time
+import signal
 from Zaid.main import start_bot
 
 
 async def main():
-    # Heroku boot time sync fix
-    await asyncio.sleep(5)
-
+    # bot start
     await start_bot()
+
+    # bot को exit होने से रोकता है
+    stop_event = asyncio.Event()
+
+    for sig in (signal.SIGINT, signal.SIGTERM):
+        asyncio.get_event_loop().add_signal_handler(
+            sig, stop_event.set
+        )
+
+    await stop_event.wait()
 
 
 if __name__ == "__main__":
